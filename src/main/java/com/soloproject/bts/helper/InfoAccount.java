e26ce7c1c2663410966c8d87304cbc4ea7722156
Laksa Bayu Trie Aji <laksabayu18@gmail.com>
2025-05-26 10:21:32 +0700

    Add comment

--- src/main/java/com/soloproject/bts/helper/InfoAccount.java
+++ src/main/java/com/soloproject/bts/helper/InfoAccount.java
@@ -7,6 +7,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
+// Kegunaannya untuk mendapatkan informasi lengkap tentang pengguna (user)
+// yang sedang masuk (login) ke aplikasi Anda, langsung dari database.
 public class InfoAccount {
 
     @Autowired
@@ -15,15 +17,15 @@ public class InfoAccount {
 
     public Users get(){
 
-        var auth =  SecurityContextHolder.getContext().getAuthentication();
-        var name = auth.getName();
-        var data = usersRepository.findByEmail(name);
+        var auth =  SecurityContextHolder.getContext().getAuthentication(); // Mendapatkan informasi otentikasi (siapa yang sedang login) dari Spring Security
+        var name = auth.getName(); // Dari informasi otentikasi, mengambil "nama" pengguna yang sedang login. Dalam banyak kasus, ini adalah username atau email.
+        var data = usersRepository.findByEmail(name); // Menggunakan "nama" (misalnya email) tadi untuk mencari data pengguna lengkap di database
 
         if(data.isEmpty()){
             return null;
         }
 
-        return data.get();
+        return data.get(); // Jika pengguna ditemukan, kembalikan objek Users lengkapnya
 
     }
 }
